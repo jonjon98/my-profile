4380018b0ddd83b5b2a8278e9df1f6c648b4033e
jonjon98 <yapzh.jon@gmail.com>
2022-05-25 22:35:00 +0800

    feat: add functionality of navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#about');
+  
   return (
-    <div>Nav</div>
+    <div>
+      <nav>
+        <a href='#about' onClick={() => setActiveNav("#about")} className={activeNav === '#about' ? "active" : ''}><AiOutlineHome /></a>      
+        <a href='#experience' onClick={() => setActiveNav("#experience")} className={activeNav === '#experience' ? "active" : ''}><AiOutlineUser /></a>     
+        <a href='#projects' onClick={() => setActiveNav("#projects")} className={activeNav === '#projects' ? "active" : ''}><BiBook /></a>      
+        <a href='#skills' onClick={() => setActiveNav("#skills")} className={activeNav === '#skills' ? "active" : ''}><RiServiceLine /></a>     
+        <a href='#contact' onClick={() => setActiveNav("#contact")} className={activeNav === '#contact' ? "active" : ''}><BiMessageSquareDetail /></a>      
+      </nav>
+  </div>
   )
 }
 
