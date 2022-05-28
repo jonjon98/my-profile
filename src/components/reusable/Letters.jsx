068fed1f990ce3d0dcec4b05cd04bc97425eecad
jonjon98 <yapzh.jon@gmail.com>
2022-05-28 11:18:34 +0800

    chore: changed some of the styling options like fonts and font sizes

--- src/components/reusable/Letters.jsx
+++ src/components/reusable/Letters.jsx
@@ -7,7 +7,7 @@ const Letters = (props) => {
   const letters = props.letters.split(/(?!$)/u);  
   const lettersItems = letters.map((letter, index) => {
       if(letter === " "){
-        return <pre> </pre>
+        return <pre>  </pre>
       }
       return <Letter key={index} letter={letter} />
     }
