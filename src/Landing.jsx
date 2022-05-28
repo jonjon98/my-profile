d6c5691a66eccab161071e81a3410782f17bc5a5
jonjon98 <yapzh.jon@gmail.com>
2022-05-28 14:10:04 +0800

    feat: add a cursor trail to the cursor in the landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import Letters from './components/reusable/Letters.jsx'
+import Cursor from './globalStyle/Cursor.jsx'
 
 const Landing = () => {
   const navigate = useNavigate();
@@ -9,17 +10,16 @@ const Landing = () => {
     navigate('/Header#about');
   }
 
+  Cursor()
+
   return (
     <>
-      <div>
-          <div>
-          <Letters letters="Hi, I'm Jonathan."></Letters>
+          <canvas id='div' />
+          <div className='foreground'>
+            <Letters letters="Hi, I'm Jonathan."></Letters>
+            <Letters letters="Looking into further developments in Cyber Security and Cloud Computing."></Letters>
+            <button onClick={handleClick} className='btn'>start</button>
           </div>
-          <div>
-          <Letters letters="Looking into further developments in Cyber Security and Cloud Computing."></Letters>
-          </div>
-          <button onClick={handleClick} className='btn'>start</button>
-      </div>
     </>
   )
 }
