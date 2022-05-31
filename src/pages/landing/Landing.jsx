1b4ae93a816f9df775ebf5bfa7e850e440ff843a
jonjon98 <yapzh.jon@gmail.com>
2022-05-31 14:03:26 +0800

    feat: adds states to check if "cd ~/About" button is pressed

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -7,10 +7,15 @@ import { AiOutlineHome } from 'react-icons/ai'
 import { VscTerminalUbuntu } from 'react-icons/vsc'
 
 const Landing = () => {
+  /* using states to check if "cd ~/About" button is pressed */
+  const [cmdState, setCmdState] = useState(0);
+
   /* functions to nagivate to header page */
   const navigate = useNavigate();
 
-  function handleClick() {
+  async function handleClick() {
+    setCmdState(cmdState+1);
+    await new Promise(r => setTimeout(r, 1500));
     navigate('/Header#about');
   }
   
@@ -18,8 +23,7 @@ const Landing = () => {
   const [animationEnd, setAnimationEnd] = useState(0);
   
   function handleAnimation() {
-    let temp = animationEnd;
-    setAnimationEnd(++temp)
+    setAnimationEnd(animationEnd+1)
   }
 
   /* function to give cursor a trail */
@@ -31,7 +35,9 @@ const Landing = () => {
       <div className='foreground'>
         <h1 className='prompt'><VscTerminalUbuntu className='OSPrompt' />/<AiOutlineHome className='dirPrompt' />~</h1>
         <h1 className={animationEnd === 0 ? "typewriter" : animationEnd > 0 ? '' : 'hidden'} onAnimationEnd={handleAnimation}>$ whoami</h1>
-        <h1 className={animationEnd === 1 ? "typewriter" : animationEnd > 1 ? '' : 'hidden'}>Jonathan Yap</h1>
+        <h1 className={animationEnd === 1 ? "typewriter" : animationEnd > 1 ? '' : 'hidden'} onAnimationEnd={handleAnimation}>Jonathan Yap</h1>
+        <h1 className={animationEnd === 2 ? 'prompt' : 'hidden'}><VscTerminalUbuntu className='OSPrompt' />/<AiOutlineHome className='dirPrompt' />~</h1>
+        <h1 className={cmdState === 1 ? "typewriter" : 'hidden'}>cd ~/About</h1>
         {/*<Letters letters="Hi, I'm Jonathan."></Letters>*/}
         {/*<Letters letters="Looking into further developments in Cyber Security and Cloud Computing."></Letters>*/}
         <button onClick={handleClick}>cd ~/About</button>
