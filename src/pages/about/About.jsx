f0a2b29e61f4fd612038415e8d98305fd7fb0d9b
jonjon98 <yapzh.jon@gmail.com>
2022-06-14 23:23:48 +0800

    feat: adds LavaFloor canvas to Landing page. Changes styling of Landing page but not completed (WIP). Moves .foreground and .hidden styling to landing.css

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -21,6 +21,8 @@ const About = () => {
             </div>
           </div>
           <div>
+            "Live as if you were to die tomorrow. Learn as if you were to live forever." -Mahatma Gandhi
+
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type 
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './landing.css'
 import Cursor from './Cursor.js'
+import LavaFloor from './LavaFloor.js'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { AiOutlineHome } from 'react-icons/ai'
-import { VscTerminalUbuntu } from 'react-icons/vsc'
+//import { AiOutlineHome } from 'react-icons/ai'
+//import { VscTerminalUbuntu } from 'react-icons/vsc'
 
 const Landing = () => {
   /* using states to check if "cd ~/About" button is pressed */
@@ -16,31 +17,41 @@ const Landing = () => {
   async function handleClick() {
     setCmdState(cmdState+1);
     /* sleep for 1.5 seconds */
-    await new Promise(r => setTimeout(r, 1500));
+    await new Promise(r => setTimeout(r, 0));
     navigate('/About', {state:{name:'About'}});
   }
   
   /* using states to check if typing animation is done */
-  const [animationEnd, setAnimationEnd] = useState(0);
+  //const [animationEnd, setAnimationEnd] = useState(0);
   
-  function handleAnimation() {
-    setAnimationEnd(animationEnd+1)
-  }
+  //function handleAnimation() {
+  //  setAnimationEnd(animationEnd+1)
+  //}
 
+  /* TRY TO MERGE THESE TWO */
   /* function to give cursor a trail */
   Cursor()
+  /* function to give LavaFloor */
+  LavaFloor()
 
   return (
     <>
       <canvas id='canvas' />
+      <canvas id='canvas2' />
+      
       <div className='foreground'>
-        <h1 className='prompt'><VscTerminalUbuntu className='OSPrompt' />/<AiOutlineHome className='dirPrompt' /> ~</h1>
-        <h1 className={animationEnd === 0 ? "typewriter" : animationEnd > 0 ? '' : 'hidden'} onAnimationEnd={handleAnimation}>$ whoami</h1>
-        <h1 className={animationEnd === 1 ? "typewriter" : animationEnd > 1 ? '' : 'hidden'} onAnimationEnd={handleAnimation}>Jonathan Yap</h1>
-        <h1 className={animationEnd === 2 ? 'prompt' : 'hidden'}><VscTerminalUbuntu className='OSPrompt' />/<AiOutlineHome className='dirPrompt' /> ~</h1>
-        <h1 className={cmdState === 1 ? "typewriter" : 'hidden'}>cd ~/About</h1>
+        {/*<h1 className='prompt'><VscTerminalUbuntu className='OSPrompt' />/<AiOutlineHome className='dirPrompt' /> ~</h1>*/}
+        {/*<h1 className={animationEnd === 0 ? "typewriter" : animationEnd > 0 ? '' : 'hidden'} onAnimationEnd={handleAnimation}>$ whoami</h1>*/}
+        {/*<h1 className={animationEnd === 1 ? "typewriter" : animationEnd > 1 ? '' : 'hidden'} onAnimationEnd={handleAnimation}>Jonathan Yap</h1>*/}
+        {/*<h1 className={animationEnd === 2 ? 'prompt' : 'hidden'}><VscTerminalUbuntu className='OSPrompt' />/<AiOutlineHome className='dirPrompt' /> ~</h1>*/}
+        {/*<h1 className={cmdState === 1 ? "typewriter" : 'hidden'}>cd ~/About</h1>*/}
         {/*<Letters letters="Hi, I'm Jonathan."></Letters>*/}
         {/*<Letters letters="Looking into further developments in Cyber Security and Cloud Computing."></Letters>*/}
+        <p class='glitch'>
+          I am Jonathan
+          <span aria-hidden='true'>I am Jonathan</span>
+          <span aria-hidden='true'>I am Jonathan</span>
+        </p>
         <button onClick={handleClick}>cd ~/About</button>
       </div>
     </>
