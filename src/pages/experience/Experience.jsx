ad1c6d1f7cdb84e1b997478f2ea506c3e176e43d
jonjon98 <yapzh.jon@gmail.com>
2022-08-02 00:38:46 +0800

    feat: adds dynamic coding of experience page by a json file.

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './experience.css'
+import Database from '../../assets/database/experience.json'
 import Nav from '../../components/nav/Nav.jsx'
 import { useLocation } from 'react-router-dom';
 
@@ -14,32 +15,16 @@ const Experience = () => {
           <h1 className='typewriter'>$cat experience</h1>
         </div>
         <div className='experienceText'>
-          <div>
-            <h2>Cyber Security Software Developer Intern @MicroSec</h2>
-            <h3 className='date'>Dec 2021 – May 2022</h3>
-            <h4>- Developed a C program to cluster logs of the same type.</h4>
-            <h4>- Developed security protocols using signed certificates and certificate revocation lists. </h4>
-            <h4>- Implemented a basic web-scraping tool to obtain information on a website in the form of a .csv file.</h4>
-            <h4>- Implemented a user activity simulation on an internal testbed network.</h4>
-            <h4>tech used</h4>
-          </div>
-          <br />
-          <div>
-            <h2>Full Stack Developer Intern @Avitron</h2>
-            <h3 className='date'>Jun 2021 – Aug 2021</h3>
-            <h4>- Developed a full stack system in a team to help manage company documents.</h4>
-            <h4>- Made use of company APIs to pull information from MySQL database</h4>
-            <h4>tech used</h4>
-          </div>
-          <br />
-          <div>
-            <h2>Project Lead/Full Stack Developer @ServerSam</h2>
-            <h3 className='date'>May 2021 – Aug 2021</h3>
-            <h4>- Led a team of three to complete deliverables set out by clients.</h4>
-            <h4>- Developed websites for E-commerce sites and businesses using HTML, CSS, ASP.Net, C#, and SQL Server Management databases.</h4>
-            <h4>- Implemented relevant APIs to the frontend such as Google Maps and Facebook pages. </h4>
-            <h4>tech used</h4>
-          </div>
+          {Database.map((exp) => (
+            <div>
+              <h2>{exp.role} @{exp.company}</h2>
+              <h3 className='date'>{exp.date}</h3>
+              {exp.desc.map((desc) => (
+              <h4>- {desc}</h4>
+              ))}
+              <h4>tech used</h4>
+            </div>
+          ))}
         </div>
       </div>
     </>
